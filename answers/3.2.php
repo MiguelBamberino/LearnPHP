655daf8cf11dc4ce2453d944b39556fd1e51c952
mikebamber <mikebamber@live.com>
2018-09-30 15:05:10 -0400

    complete

--- answers/3.2.php
+++ answers/3.2.php
@@ -11,12 +11,16 @@ $board['X1Y3'] = " ";
 $board['X2Y3'] = " ";
 $board['X3Y3'] = " ";
 
+print "------------------------\n";
 print "BOARD\n";
 print "------------------------\n";
 print "Y \n";
 print "3 | ".$board['X1Y3']." | ".$board['X2Y3']." | ".$board['X2Y3']." |\n";
 print "---------------\n";
-print "2 | ".$board['X1Y3']." | ".$board['X2Y3']." | ".$board['X2Y3']." |\n";
-
+print "2 | ".$board['X1Y2']." | ".$board['X2Y2']." | ".$board['X2Y2']." |\n";
 print "---------------\n";
+print "1 | ".$board['X1Y1']." | ".$board['X2Y1']." | ".$board['X2Y1']." |\n";
+print "---------------\n";
+print "X | 1 | 2 | 3 |\n";
+
 
